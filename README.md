0cc38ebde42026692831cde2746cf7517cbeb06a
Aman Kumar <amankumar8675@gmail.com>
2023-09-27 23:12:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dom-Manupulation_practice2
+ Dom-Manupulation_practice2
---
+++ script.js
@@ -0,0 +1,11 @@
+var hea =document.querySelector("i")
+var con = document.querySelector("#container")
+con.addEventListener("dblclick",function(){
+    hea.style.transform="translate(-50%,-50%)scale(150%)";
+    console.log("nope")
+    setTimeout
+    (function () {
+        hea.style.opacity=0
+    },2000)
+    }
+)
